b8fd8c42fb0f7b432102192d4e6e52cc3eede545
FerMeza <30847755+FerMeza@users.noreply.github.com>
2025-02-11 21:00:48 -0600

    feat: init black screen

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import pygame
 from constants import *
+import pygame
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    num_pass, num_fail = pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Game Loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(pygame.Color(0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
